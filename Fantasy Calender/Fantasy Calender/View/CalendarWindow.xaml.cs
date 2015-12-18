20ef0428ec9fbb69f90546fca61003834f56313a
Adnan Dervisevic <darthozzan@gmail.com>
2015-12-19 00:16:09 +0100

    First commit, just some basic structure and start of layout

---
+++ Fantasy Calender/Fantasy Calender/View/CalendarWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Fantasy_Calender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class CalendarWindow : Window
+    {
+        public CalendarWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Fantasy Calender/Fantasy Calender/ViewModels/CalendarViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_Calender.ViewModels
+{
+    class CalendarViewModel
+    {
+        private DateTime displayDate;
+
+        public DateTime DisplayDate
+        {
+            get
+            {
+                return displayDate;
+            }
+            set
+            {
+                if(value != displayDate)
+                {
+                    displayDate = value;
+                }
+            }
+        }
+            
+    }
+}
